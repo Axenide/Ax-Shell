ebc6a3f6ebf7b1b88ae917aa778a50af34133c16
tr1x_em <admin@trix.is-a.dev>
2025-03-07 11:23:06 +0530

    Hotfix

--- modules/controls.py
+++ modules/controls.py
@@ -192,12 +192,10 @@ class VolumeSmall(Box):
     def on_scroll(self, _, event):
         if not self.audio.speaker:
             return
-
-        if event.direction == Gdk.ScrollDirection.SMOOTH:
-            if abs(event.delta_y) > 0:
-                self.audio.speaker.volume += event.delta_y
-            if abs(event.delta_x) > 0:
-                self.audio.speaker.volume -= event.delta_x
+        if abs(event.delta_y) > 0:
+            self.audio.speaker.volume -= event.delta_x
+        if abs(event.delta_x) > 0:
+            self.audio.speaker.volume += event.delta_y
 
         # match event.direction:
         #     case 0:
@@ -274,23 +272,13 @@ class MicSmall(Box):
                 self.mic_label.remove_style_class("muted")
 
     def on_scroll(self, _, event):
+        if not self.audio.microphone:
+            return
+        if abs(event.delta_y) > 0:
+            self.audio.microphone.volume -= event.delta_x
+        if abs(event.delta_x) > 0:
+            self.audio.microphone.volume += event.delta_y
 
-        val_y = event.delta_y
-        if val_y > 0:
-            self.audio.microphone.volume -= 1
-        else:
-            self.audio.microphone.volume += 1
-        return
-        # match event.direction:
-    #
-    #     # if not self.audio.microphone:
-    #     #     return
-    #     # match event.direction:
-    #     #     case 0:
-    #     #         self.audio.microphone.volume += 1
-    #     #     case 1:
-    #     #         self.audio.microphone.volume -= 1
-    #
     def on_microphone_changed(self, *_):
         if not self.audio.microphone:
             return
@@ -338,13 +326,13 @@ class BrightnessSmall(Box):
         self.on_brightness_changed()
 
     def on_scroll(self, _, event):
+        if self.brightness.max_screen == -1:
+            return
         val_y = event.delta_y
         if val_y > 0:
             self.brightness.screen_brightness -= 1
         else:
             self.brightness.screen_brightness += 1
-        # if self.brightness.max_screen == -1:
-        #     return
         # match event.direction:
         #     case 0:
         #         self.brightness.screen_brightness += 10  # Increment brightness
