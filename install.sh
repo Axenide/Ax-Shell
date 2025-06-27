d302aa9c0e0e2aabb070b969b52864e38697e667
pundemia-dev <pundemia@gmail.com>
2025-06-28 04:19:01 +0500

    install script fix

--- install.sh
+++ install.sh
@@ -117,6 +117,7 @@ fi
 
 cd "$INSTALL_DIR"
 uv sync
+uv pip uninstall fabric
 uv pip install git+https://github.com/Fabric-Development/fabric
 uv pip uninstall PyGObject
 uv pip install PyGObject # TODO: Move the PyGObject installation into pyproject once its required version becomes available in UV.
